eea551480a246edd1f9e79f2c2e395c861c050eb
kibet <kmishmael@gmail.com>
2024-04-14 16:19:22 +0300

    test: dynamic opengraph image generation

--- src/pages/generateImage.ts
+++ src/pages/generateImage.ts
@@ -1,4 +1,22 @@
+import { toBlob } from 'html-to-image';
+import * as jsdom from "jsdom";
+const { JSDOM } = jsdom;
+
 export async function GET({ params, request }) {
-    const response = await fetch("https://docs.astro.build/assets/full-logo-dark.png");
-    return new Response(await response.arrayBuffer());
+
+    const c = new JSDOM()
+    const document = c.window._document
+    const node: HTMLElement = document.createElement('div');
+    node.innerHTML = `
+    <div class="canves"></div>
+    <style>
+        .canvas {
+            height: 300px;
+            width: 400px;
+            background-color: red;
+        }
+    </style>
+    `
+    const response = await toBlob(node)
+    return new Response(response);
 }
