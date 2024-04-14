ce19f2065349e5bd2a1d6bc3ecc86cc3de5b7685
kibet <kmishmael@gmail.com>
2024-04-14 13:49:33 +0300

    chore: ui adjustment to improve responsiveness

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"primary": "#008ecc",
+				"light-grey": "#b5b5b5",
+				"dark-grey": "#919191",
+				"primary2": "#f3f9fb",
+				"secondary": "#f5f5f5"
+			}
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
