0be3795045e1d20003a1f7f7e4c3ef2131cd9765
kibet <kmishmael@gmail.com>
2024-04-20 00:59:23 +0300

    chore: add sitemap and robots file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import metaTags from "astro-meta-tags";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), metaTags()],
-  site: 'https://kmishmael.tech'
+  integrations: [tailwind(), metaTags(), sitemap()],
+  site: 'https://kmishmael.tech',
+  trailingSlash: 'never',
 });
